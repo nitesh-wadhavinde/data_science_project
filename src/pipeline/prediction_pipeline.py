fc6d0e58deb49ac75672a08936845bd972027af9
nitesh-wadhavinde <niteshwadhavinde.nw@gmail.com>
2024-07-27 09:29:11 +0530

    Deployment configuration AWS Elastic,Azure after resolving error of accessing pkl file

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
@@ -11,9 +12,13 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            #model_path='artifacts\model.pkl'
+            model_path=os.path.join("artifacts","model.pkl")
+            #preprocessor_path='artifacts\preprocessor.pkl'
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
             model=load_object(file_path=model_path)
+            print("After Loading")
             preprocessor=load_object(file_path=preprocessor_path)
 
             data_scaled=preprocessor.transform(features)
